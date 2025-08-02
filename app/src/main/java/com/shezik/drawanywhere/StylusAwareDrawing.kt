1a2c0cbe0b73e384ceee92eeb261014d86ea195a
Kerry Shen <kefishen@gmail.com>
2025-08-02 17:48:24 +0800

    StylusAwareDrawing: Stylus awareness

--- app/src/main/java/com/shezik/drawanywhere/StylusAwareDrawing.kt
+++ app/src/main/java/com/shezik/drawanywhere/StylusAwareDrawing.kt
@@ -1,32 +1,50 @@
 package com.shezik.drawanywhere
 
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.PointerId
+import androidx.compose.ui.input.pointer.PointerType
+import androidx.compose.ui.input.pointer.isPrimaryPressed
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChanged
 
 fun Modifier.stylusAwareDrawing(
     controller: DrawController
 ): Modifier = pointerInput(Unit) {
-    awaitPointerEventScope {
-        var prevPressId: PointerId? = null
+    awaitEachGesture {
+        val initialDown = awaitFirstDown()
+
+        val initialEvent = awaitPointerEvent()
+        val initialChange = initialEvent.changes.firstOrNull { it.id == initialDown.id }
+
+        // Just maybe.
+        if (initialChange == null || !initialChange.pressed)
+            return@awaitEachGesture
+
+        val isEraser = initialDown.type == PointerType.Stylus && initialEvent.buttons.isPrimaryPressed
+
+        if (isEraser) {
+            // TODO: controller eraser method
+        } else {
+            controller.createPath(initialDown.position)
+            controller.updateLatestPath(initialChange.position)
+        }
+        initialChange.consume()
+
         while (true) {
             val event = awaitPointerEvent()
-            val change = event.changes.firstOrNull() ?: continue
+            val change = event.changes.firstOrNull { it.id == initialDown.id }
 
-            val id = change.id
-            val pos = change.position
+            if (change == null || !change.pressed)
+                break
 
-            if (change.pressed) {
-                if (event.type == PointerEventType.Press || id != prevPressId) {
-                    controller.createPath(pos)
-                    prevPressId = id
+            if (change.positionChanged()) {
+                if (isEraser) {
+                    // TODO: controller eraser method
                 } else {
-                    controller.updateLatestPath(pos)
+                    controller.updateLatestPath(change.position)
                 }
                 change.consume()
-            } else {
-                prevPressId = null
             }
         }
     }
